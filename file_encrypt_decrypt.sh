4060753eda2b54bc2596c8a044bab0f8c862a112
Yash Tangadi <yashtangadi1999@gmail.com>
2024-04-28 01:03:03 +0530

    Final changes made

---
+++ file_encrypt_decrypt.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+#BASH SCRIPT TO ENCRYPT AND DECRYPT A FILE IN LINUX
+
+echo "SCRIPT TO ENCRYPT AND DECRYPT A FILE"
+echo "Please select mode : "
+
+choice="Encrypt Decrypt"
+select option in $choice
+do
+	if [ $REPLY = 1 ]
+	then 
+		echo "Please Enter The Filename You Want To Encrypt : " 
+		read file
+		: '
+			GNU Privacy Guard (GPG, also GnuPG) is free encryption software
+			that is compliant with the OpenPGP (RFC4880) standard. ... 
+			To use GPG, you create a unique encryption key, and 
+			then use that key to encrypt and decrypt your files.
+		'
+		gpg -c $file
+		echo "File - $file  Has Been Encrypted"
+	else 
+		echo "Please Enter The Filename You Want To Decrypt : "
+		read file
+		gpg -d $file
+		echo "File - $file Has Been Decrypted"
+	fi
+done
